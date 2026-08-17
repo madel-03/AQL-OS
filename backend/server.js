f28e31b31328802e8817fdc951c78b1a190b8d45
madel-03 <m5726738@gmail.com>
2026-08-17 15:10:03 +0300

    Allow Vercel origin in CORS

--- backend/server.js
+++ backend/server.js
@@ -10,13 +10,14 @@ import os from 'os';
 const app = express();
 
 app.use(cors({
-  origin: [
-    'http://localhost:5173',
-    'http://localhost:5174',
-    'http://127.0.0.1:5173',
-    'http://127.0.0.1:5174',
-    'https://aql-os-orcin.vercel.app/',  
-  ],
+origin: [
+'http://localhost:5173',
+'http://localhost:5174',
+'http://127.0.0.1:5173',
+'http://127.0.0.1:5174',
+'https://aql-os-orcin.vercel.app',
+/^https:\/\/.*\.vercel\.app$/,
+],
 }));
 app.use(express.json());
 
